c4f2705f415433b206080e785e30e3282356ae00
maxzastavny <max.xas>
2021-09-26 19:29:53 +0200

    add main class and doctest

--- src/udemy/Session3/ContainerWithMostWater.py
+++ src/udemy/Session3/ContainerWithMostWater.py
@@ -5,3 +5,24 @@
 # together with the x-axis forms a container, such that the container contains the most water.
 #
 # Notice that you may not slant the container.
+
+from typing import List
+
+
+class Solution:
+    """"
+    >>> res = Solution()
+
+    >>> res.maxArea([4,2,3,4])
+    3
+
+    >>> res.maxArea([1,8,6,2,5,4,8,3,7])
+    49
+    """
+    def maxArea(self, height: List[int]) -> int:
+        return 3
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
