d86d827b2da74d12be077ad29a0bc8e23f7d9db2
Max <max.zastavny@gmail.com>
2021-12-19 16:00:20 +0100

    own solution

--- src/udemy/Session5/LongestUniqueSubstring.py
+++ src/udemy/Session5/LongestUniqueSubstring.py
@@ -1,17 +1,41 @@
 from doctest import testmod
 
 
-def longest_unique_sequence(str):
-    '''
-    >>> longest_unique_sequence("adccdb")
-    3
-    >>> longest_unique_sequence('ccccccc')
-    1
-    >>> longest_unique_sequence("adcdba")
-    4
-    '''
-    pass
+class LongestUniqueSequence:
+    def __init__(self):
+        self.stored = set()
+        self.max_len = 0
+        self.p = 0
+
+    def find_len(self, s):
+        '''
+        >>> t = LongestUniqueSequence()
+        >>> t.find_len('abccdb')
+        3
+        >>> t = LongestUniqueSequence()
+        >>> t.find_len('cccccccccccc')
+        1
+        >>> t = LongestUniqueSequence()
+        >>> t.find_len("adcdba")
+        4
+        '''
+        for i in range(len(s)):
+            if s[i] in self.stored:
+                self.remove_until(s, self.p, i)
+            else:
+                self.stored.add(s[i])
+                self.max_len = max(self.max_len, len(self.stored))
+
+        return self.max_len
+
+    def remove_until(self, s, p0, p1):
+        for i in range(p0, p1):
+            if s[i] != s[p1]:
+                self.stored.remove(s[i])
+            else:
+                self.p = i+1
+                break
 
 
 if __name__ == '__main__':
-    testmod(name='longest_unique_sequence', verbose=True)
+    testmod()
